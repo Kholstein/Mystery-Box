0107dc4138d16a3da48a4d8f3c2d6ffa6f8d5660
Misael C_programmer <misaelcerda@yahoo.com>
2019-04-08 19:28:43 -0400

    week11
    My work for week 11

---
+++ Mystery Box/Assets/_Scripts/Bullet.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float moveSpeed = 7f;
+    Rigidbody rb;
+    public Transform player;
+    Vector3 moveDirection;
+    private Vector3 target;
+
+    private void Start()
+    { //might need to change the axis for this piece of code since the camera will be in a 2D view
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector3(player.position.x, player.position.y);
+        moveDirection = (player.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector3(moveDirection.x, moveDirection.y);
+        //target = GameObject.Find("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        //Vector3 targetPosition = new Vector3(transform.position.x, target.transform.position.y, target.transform.position.z);
+        //transform.LookAt(targetPosition);
+
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
+
+//Ignore all the code in green, I was testing different ways to get the bullet to follow the player
+
+//   public float speed = 7f;
+//   private Transform player;
+//   private Vector3 target;
+
+//void Start () {
+//       player = GameObject.FindGameObjectWithTag("Player").transform;
+//       target = new Vector3(player.position.x, player.position.z);
+//}
+
+//void Update () {
+//       transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+//       if (transform.position.x == target.x && transform.position.z == target.z)
+//       {
+//           DestroyProjectile();
+//       }
+//}
+
+//   //private void OnTriggerEnter(Collider other)
+//   //{
+//   //    if (other.CompareTag("Player"))
+//   //    {
+//   //        DestroyProjectile();
+//   //    }
+//   //}
+
+//   void DestroyProjectile()
+//   {
+//       Destroy(gameObject);
+//   }
---
+++ Mystery Box/Assets/_Scripts/Enemy.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed;
+    //private Rigidbody rb;
+    public Transform player;
+    public float stoppingDistance;
+    public float retreatDistance;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    public GameObject projectile;
+    GameObject target;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+        target = GameObject.Find("Player");
+    }
+
+    //void FixedUpdate()
+    //{
+    //    Vector3 vel = rb.velocity;
+    //    rb.velocity = vel;
+    //}
+
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(transform.position.x, target.transform.position.y, target.transform.position.z);
+        transform.LookAt(targetPosition);
+
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
+
+//This code is if we would want the shooting enemy to move
+
+//if (Vector3.Distance(transform.position, player.position) > stoppingDistance)
+//{
+//    transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+//}
+//else if (Vector3.Distance(transform.position, player.position) < stoppingDistance && Vector3.Distance(transform.position, player.position) > retreatDistance)
+//{
+//    transform.position = this.transform.position;
+//}
+//else if (Vector3.Distance(transform.position, player.position) < retreatDistance)
+//{
+//    transform.position = Vector3.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+//}
+
+//this code is for enemy rotation, I couldn't get it to work
+//   private float speed = 30;
+//   public Rigidbody throwable;
+//   RaycastHit hit;
+
+//   public float fieldOfViewAngle = 110f;
+//   public bool playerInSight;
+//   private SphereCollider col;
+//   private GameObject player;
+
+//   private void Awake()
+//   {
+//       if (hit.collider.gameObject == player)
+//       {
+//           playerInSight = true;
+//           col = GetComponent<SphereCollider>();
+//           player = GameObject.FindGameObjectWithTag("Player");
+//       }
+//   }
+
+
+//   private void OnTriggerStay(Collider other)
+//   {
+//       if(other.gameObject == player)
+//       {
+//           playerInSight = false;
+//           Vector3 direction = other.transform.position - transform.position;
+//           float angle = Vector3.Angle(direction, transform.forward);
+
+//           if(angle < fieldOfViewAngle * 0.05f)
+//           {
+//               RaycastHit hit;
+//               if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius))
+//               {
+//                   if(hit.collider.gameObject == player)
+//                   {
+//                       playerInSight = true;
+//                   }
+//               }
+//           }
+//       }
+//   }
+
+//   private void OnTriggerExit(Collider other)
+//   {
+//       if(other.gameObject == player)
+//       {
+//           playerInSight = false;
+//       }
+//   }

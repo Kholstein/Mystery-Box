ffb5810a15c67541bbf22c4b417be7e2444c9706
FoldingBoxes <47989087+FoldingBoxes@users.noreply.github.com>
2019-04-09 21:29:29 -0400

    Add files via upload

---
+++ Mystery Box/Assets/_Scripts/DeathWall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DeathWall : MonoBehaviour
+{
+
+    public float speed;
+    public Transform respawnPosition;
+
+    void FixedUpdate()
+    {
+
+        transform.position += Vector3.forward * Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) //Large death trigger
+        {
+            SceneManager.LoadScene("LevelOne");
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        transform.position = respawnPosition.position;
+        
+    }
+}

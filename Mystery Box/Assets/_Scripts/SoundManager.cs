da894c7e37ba9568f03cbb4084702216fa497d44
FoldingBoxes <47989087+FoldingBoxes@users.noreply.github.com>
2019-04-30 17:19:56 -0400

    Add files via upload

---
+++ Mystery Box/Assets/_Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public float time = 30f; //30 seconds for you
+
+    public void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("Play Audio Here -- Timer Over!!");
+            GetComponent<AudioSource>().Play();
+        }
+
+
+    }
+}
